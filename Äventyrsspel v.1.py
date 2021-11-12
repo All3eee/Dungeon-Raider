d03ce01272461a38a3e411d2a08c52dbc81b914e
All3eee <alvin.havren@skola.taby.se>
2021-11-12 10:42:11 +0100

    Co-authored-by: jompis <jompis@users.noreply.github.com>

--- Äventyrsspel v.1.py
+++ Äventyrsspel v.1.py
@@ -1,1 +1,31 @@
-print("hej")
+
+import sys
+import time
+
+def meny():
+    string ='''
+-----------------------------
+Välkommen till Dungeon Raider
+-----------------------------'''
+    for char in string:
+        sys.stdout.write(char)
+        sys.stdout.flush()
+        time.sleep(.03)
+
+class Player():
+    def __init__(self, name, strenght, hp, lvl):
+        self.name = name
+        self.strenght = strenght
+        self.hp = hp
+        self.lvl = lvl
+
+class Item():
+    def __init(self, strenght_bonus):
+        self.strenght_bonus = strenght_bonus
+
+
+#Huvudprogram
+
+while True:
+    meny()
+    break
