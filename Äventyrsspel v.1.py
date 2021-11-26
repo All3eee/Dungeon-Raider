60e554da6dc4a34a743475159054acf8ff6c71a9
All3eee <alvin.havren@skola.taby.se>
2021-11-26 11:17:41 +0100

    Co-authored-by: Jonathan Lavén <JonathanBoog@users.noreply.github.com> Co-authored-by: jompis <jompis@users.noreply.github.com>

--- Äventyrsspel v.1.py
+++ Äventyrsspel v.1.py
@@ -1,32 +1,42 @@
 #Använd sleep() istället för time.sleep()
 #Man kan berätta i början hur man öppnar inventory etc.
-
+from Functions import animation
 import random as rand
 import sys
 from time import sleep
 
 class Player():
-    def __init__(self, name, strength, hp, lvl, respawn, inventory):
+    def __init__(self, name, strength, hp, lvl, lives, inventory):
         self.name = name
         self.strength = strength
         self.hp = hp
         self.lvl = lvl
-        self.respawn = respawn
+        self.lives = lives
         self.inventory = inventory
+
+    def inventory():
+        pass
+    
+    def abilites(self):
+        print(f'''
+        Strength: {self.strength}
+        Level: {self.lvl}
+        Lives: {self.lives}
+        
+        ''')
     
     def difficulty(self):
         print("Du kommer nu att få bestämma förinställningar")
         while True:
-            respawn_on_off = input('''
-        Ska respawn vara på eller av?
-        På [1]
-        Av [2]
+            difficulty = input('''
+                Svårighetsgrad
+        Normal[1]           Hardcore[2]
         ---> ''')
-            if respawn_on_off == '1':
-                self.respawn = True
+            if difficulty == '1':
+                self.lives = 3
                 break
-            elif respawn_on_off == '2':
-                self.respawn = False
+            elif difficulty == '2':
+                self.lives = 1
                 break
             else:
                 print("\nAnge [1] eller [2]")
@@ -40,9 +50,27 @@ Laban - Jasså, så du heter {self.name}.
         ''')
 
 class Item():
-    def __init(self, type_of_item, strength_bonus):
+    def __init__(self, type_of_item, strength_bonus):
         self.type_of_item = type_of_item
         self.strength_bonus = strength_bonus
+    
+    def item_type_decider():
+        type = rand.randint(1, 100)
+        if type == (1, 40):
+            type_of_item = "sword"
+        elif type == (41, 60):
+            type_of_item = "ring"
+        elif type == (61, 100):
+            type_of_item = "potion"
+
+    def item_type_sword():
+        rarity = rand.randint(1, 100)
+        if rarity == (1, 50):
+            return(wooden_sword)
+        if rarity == (51, 100):
+            return(laser_saber)
+
+
 
 def start_game():
     string ='''
@@ -78,19 +106,6 @@ def meny():
 
 def the_room():
     while True:
-        print('''
- ____________   ____________   ____________
-|            | |            | |            |
-|            | |            | |            |
-|            | |            | |            |
-|      V     | |      M     | |      H     | 
-|            | |            | |            |
-|            | |            | |            |
-|____________| |____________| |____________|
-    
-Vänster dörr [V], Mitten dörr [M], Höger dörr [H]    
-Meny [E]  
-        ''')
         chosen_input = input("Ange här --> ")
         chosen_input = chosen_input.lower()
         if chosen_input == 'v' or 'm' or 'h' or 'e':
@@ -99,18 +114,21 @@ Meny [E]
 def door_chance():
     pass
 
-#ska denna vara som en metod i klassen Person?
-def inventory():
+def room_chest():
+    pass
+    
+def room_trap():
+    print("Oh no! It's a trap")
+    
+def room_monster():
     pass
 
-def egenskaper():
-    pass
-        
+def boss_monster():
+    pass        
 
-#Huvudprogram
-
-Player1 = Player('x',10,200,0, True, [])
-
+#Main Program
+Player1 = Player('x',10,200,0, 3, [])
+animation()
 
 start_game()
 Player1.difficulty()
@@ -120,13 +138,30 @@ Prolog()
 Player1.set_character()
 
 while True:
+    if Player1[0] == 10:
+        break
     given_input = the_room()
     if given_input == 'e':
-        meny()
+        while True:
+            chosen_number = meny()
+            if chosen_number == '1':
+                abilities():
+            elif chosen number == '2':
+                Player1.inventory():
     elif given_input == 'v':
         print("Vänster dörr öppnas")
+        room_type = door_chance()
+        if room_type == 1:
+            room_chest()
+        elif room_type == 2:
+            room_monster()
+        elif room_type == 3:
+            room_trap()
     elif given_input == 'm':
         print("Dörren i mitten öppnas")
     elif given_input == 'h':
         print("Höger dörr öppnas")
-    break
+
+boss_monster()
+while True:
+    pass
