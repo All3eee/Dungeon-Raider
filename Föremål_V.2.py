88529a501c26073898a2f5a4791af3da1c122c38
Alvin Havren <alvin.havren@gmail.com>
2021-11-26 20:05:43 +0100

    file update

--- Föremål_V.2.py
+++ Föremål_V.2.py
@@ -1,21 +1,29 @@
 import random
-
-numberList = [111, 222, 333, 444, 555]
-print(random.choices(numberList, weights=(10, 10, 10, 10, 60), k=5 ))
+from types import new_class
 
 
-katigories = [sword, potion, ring]
-sword_items = ["woodensword", "lightsabre"]
+katigories = ['sword', 'potion', 'ring']
+sword_items = ["woodensword", "lightsaber"]
 
 def item_type_decider():
-    item_type = random.choices(katigories, weights=(40, 20, 40), k=2 ))
+    item_type = random.choices(katigories, weights=(40, 20, 40), k=1 )
     return item_type
+
+def item_kategory_sword():
+    item_sword = random.choices(sword_items, weights=(50, 50), k=1)
+    return item_sword
 
 
 new_item_kategory = item_type_decider()
+new_sword_item = item_kategory_sword()
+h = new_sword_item.pop(0)
+print(h)
 
-if 
+swords = {
+    "woodensword": 2,
+    "lightsaber": 1000,
+}
 
-
-
-
+s = 0
+s += swords[h]
+print(s)
