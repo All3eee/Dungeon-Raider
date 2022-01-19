0def25ea00f9d2f0f07d730c95d0dafc335a43dc
Jonathan Lavén <jonathan.laven123@gmail.com>
2022-01-19 19:14:04 +0100

    yes

--- Animationer.py
+++ Animationer.py
@@ -50,7 +50,7 @@ def monster_animation():
   '''
   slumpar ett tal, för att sedan printa ett visst monster, + returnar namnet på monstret
   '''
-  monster_type = rand.randint(1,5)
+  monster_type = rand.randint(1,6)
 
   #Monstret Jörgen
   if monster_type == 1:
